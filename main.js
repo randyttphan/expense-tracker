a8fbb07e598c585d4af233d04eec6d5dce4fc542
randyttphan <randyttphan@gmail.com>
2020-03-03 12:40:37 -0600

    persisting data to local storage

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const dummyTransactions = [
-  { id: 1, text: "Flower", amount: -20 },
-  { id: 2, text: "Salary", amount: 300 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 }
-];
+// const dummyTransactions = [
+//   { id: 1, text: "Flower", amount: -20 },
+//   { id: 2, text: "Salary", amount: 300 },
+//   { id: 3, text: "Book", amount: -10 },
+//   { id: 4, text: "Camera", amount: 150 }
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
 //Adding transaction
 function addTransaction(e) {
@@ -33,6 +38,8 @@ function addTransaction(e) {
 
     //update our values
     updateValues();
+    updateLocalStorage();
+
     text.value = "";
     amount.value = "";
   }
@@ -88,8 +95,13 @@ function updateValues() {
 //Remove transaction by ID
 function removeTransaction(id) {
   transactions = transactions.filter(transaction => transaction.id !== id);
-
+  updateLocalStorage();
   init();
+}
+
+//Update local storage transactions
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
 
 //Init app
